1487fc8cdb504d2296a13a7b07d73b162585b7a5
DShamak <dmitry.shamak.dev@gmail.com>
2023-11-09 07:17:08 +0400

    Resolve build path

--- webpack.config.js
+++ webpack.config.js
@@ -3,61 +3,66 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const buildForlder = "docs";
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    chunkFilename: "[name].[contenthash].js",
-    filename: "[name].[contenthash].js",
-    assetModuleFilename: "[name].[contenthash][ext][query]",
-    asyncChunks: true,
-    path: path.resolve(__dirname, buildForlder),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, buildForlder),
+module.exports = (env) => {
+  const isDevMode = !env.production;
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      chunkFilename: "[name].[contenthash].js",
+      filename: "[name].[contenthash].js",
+      assetModuleFilename: "[name].[contenthash][ext][query]",
+      asyncChunks: true,
+      path: path.resolve(__dirname, buildForlder),
+      publicPath: isDevMode ? "/" : "./",
     },
-    port: 3003,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-      favicon: path.join(__dirname, "assets", "favicon.png")
-    }),
-  ],
-  module: {
-    // exclude node_modules
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
+    devServer: {
+      static: {
+        directory: path.join(__dirname, buildForlder),
       },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
+      port: 3003,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "src", "index.html"),
+        favicon: path.join(__dirname, "assets", "favicon.png"),
+      }),
     ],
-  },
-  resolve: {
-    alias: {
-      src: "/src",
+    module: {
+      // exclude node_modules
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+      ],
     },
-    modules: [path.resolve("./node_modules"), path.resolve("./src")],
-    extensions: [".*", ".js", ".ts", ".tsx"],
-  },
-  stats: {
-    children: true,
-  },
-  optimization: {
-    usedExports: false,
-    minimize: true,
-    splitChunks: {
-      chunks: "async",
+    resolve: {
+      alias: {
+        src: "/src",
+      },
+      modules: [path.resolve("./node_modules"), path.resolve("./src")],
+      extensions: [".*", ".js", ".ts", ".tsx"],
     },
-  },
+    stats: {
+      children: true,
+    },
+    optimization: {
+      usedExports: false,
+      minimize: true,
+      splitChunks: {
+        chunks: "async",
+      },
+    },
+  };
 };
